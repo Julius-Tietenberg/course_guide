c27c6eac49b278bd186d38f8481c0610903a30b7
Boby024 <38843651+Boby024@users.noreply.github.com>
2022-06-22 02:53:02 +0200

    Revert "5 frontend create login and register page"

--- frontend/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import movieSlice from "./features/loginSlice"
-
-export const store = configureStore({
-  reducer: {
-    movie: movieSlice
-  }
-})
